59a25b6cf3114e8aaf895ef5e1b103becf6f3a34
aaronorelowitz <aaronorelowitz1998@gmail.com>
2014-11-29 23:37:56 -0500

    Update Driver.java

--- 03wordsearch/Driver.java
+++ 03wordsearch/Driver.java
@@ -7,7 +7,10 @@ public class Driver{
 
     public static void main(String[]args)throws FileNotFoundException{
 	WordGrid A = new WordGrid(Integer.valueOf(args[0]), Integer.valueOf(args[1]));
-	A.loadWordsFromFile("WordGrid.txt", Integer.valueOf(args[3]) != 1);
+	if(args.length > 2){  
+	    A.setSeed(Long.parseLong( args[2], 10));  
+	}
+	A.loadWordsFromFile("WordGrid.txt", (args.length <= 3 || Integer.valueOf(args[3]) != 1));
 	System.out.println( "Find these words:\n"+ A.wordsInPuzzle() );
 	System.out.println(A);
     }
