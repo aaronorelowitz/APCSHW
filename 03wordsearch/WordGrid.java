469226b3892d3ba36fa607aa86597061820f400b
aaronorelowitz <aaronorelowitz1998@gmail.com>
2014-11-29 21:43:47 -0500

    Update WordGrid.java

--- 03wordsearch/WordGrid.java
+++ 03wordsearch/WordGrid.java
@@ -3,12 +3,13 @@ import java.io.FileNotFoundException;
 import java.util.*;
 public class WordGrid{
     private char[][]data;
+    private Random random = new Random();
 
     public WordGrid(int rows,int cols){
 	data = new char[rows][cols];
 	clear();
     }
- 
+
     private void clear(){
 	for(int k = 0;k < data.length; k++){
 	    for (int j = 0; j < data[k].length; j++){
@@ -29,109 +30,50 @@ public class WordGrid{
 	return ans;
     }
 
-    public boolean addWordHorizontal(String word,int row, int col){
-	if (data[row].length - col < word.length()){
-	    return false;}
-	else{
-	    for (int i = 0;i < word.length(); i++){
-		if (data[row][col + i] != ' ' && data[row][col + i] != word.charAt(i))
-		    {return false;}}
-	    for (int k = 0;k < word.length(); k++){
-		data[row][col + k] = word.charAt(k);
-	    }
-	    return true;}
 
+    public boolean addRandom( String word, int row, int col, int dx, int dy){
+	if (checkword(word ,row, col, dx, dy)){
+		for(int i = 0; i < word.length(); i++){
+		    data[row][col] = word.charAt(i);
+		    row = row + dy;
+		    col = col + dx;}
+		return true;}
+	else{return false;}
     }
 
-    public boolean addWordVertical(String word,int row, int col){
-	if (data[col].length - row < word.length()){
-	    return false;}
-	else{
-	    for (int i = 0;i < word.length(); i++){
-		if (data[row + i][col] != ' ' && data[row + i][col] != word.charAt(i))
+    public boolean checkword( String word, int row, int col, int dx, int dy){
+	if(dx == 0 && dy == 0 ||
+	   row < 0 || col < 0 ||
+	   col + dx * word.length() >= data[0].length || col + dx * word.length() < 0||
+	   row + dy * word.length() >= data.length || row + dy * word.length() < 0)
+	    return false;
+	 for (int i = 0;i < word.length(); i++){
+	     if (data[row + (i * dy)][col + (i * dx)] != ' ' && data[row + (i * dy)][col + (i * dx)] != word.charAt(i))
 		    {return false;}}
-	    for (int k = 0;k < word.length(); k++){
-		data[row + k][col] = word.charAt(k);
-	    }
-	    return true;}
-    }
+	return true;}
 
-      public boolean addWordDiagonal(String word,int row, int col){
-	if (data[col].length - row < word.length() || data[row].length - col < word.length()){
-	    return false;}
-	else{
-	    for (int i = 0;i < word.length(); i++){
-		if (data[row + i][col + i] != ' ' && data[row + i][col + i] != word.charAt(i))
-		    {return false;}}
-	    for (int k = 0;k < word.length(); k++){
-		data[row + k][col + k] = word.charAt(k);
-	    }
-	    return true;}
-    }
-
-    public boolean addWordHorizontalReverse(String word,int row, int col){
-	if (col < word.length()){
-	    return false;}
-	else{
-	    for (int i = 0;i < word.length(); i++){
-		if (data[row][col - i] != ' ' && data[row][col - i] != word.charAt(i))
-		    {return false;}}
-	    for (int k = 0;k < word.length(); k++){
-		data[row][col - k] = word.charAt(k);
-	    }
-	    return true;}
-
-    }
-
-    public boolean addWordDiagonalReverse(String word,int row, int col){
-	if (row < word.length() || col < word.length()){
-	    return false;}
-	else{
-	    for (int i = 0;i < word.length(); i++){
-		if (data[row - i][col - i] != ' ' && data[row - i][col - i] != word.charAt(i))
-		    {return false;}}
-	    for (int k = 0;k < word.length(); k++){
-		data[row - k][col - k] = word.charAt(k);
-	    }
-	    return true;}
-    }
-
-    public boolean addWordVerticalReverse(String word,int row, int col){
-	if (row < word.length()){
-	    return false;}
-	else{
-	    for (int i = 0;i < word.length(); i++){
-		if (data[row - i][col] != ' ' && data[row - i][col] != word.charAt(i))
-		    {return false;}}
-	    for (int k = 0;k < word.length(); k++){
-		data[row - k][col] = word.charAt(k);
-	    }
-	    return true;}
-    }
-
-    public boolean addRandom(String word,int row, int col){
-	Random R = new Random();
-	int X = R.nextInt(6);
-	if (X == 0)return addWordHorizontal(word, row, col);
-	if (X == 1)return addWordVertical(word, row, col);
-	if (X == 2)return addWordDiagonal(word, row, col);
-	if (X == 3)return addWordHorizontalReverse(word, row, col);
-	if (X == 4)return addWordVerticalReverse(word, row, col);
-	else{return addWordDiagonalReverse(word, row, col);}
-    }				   
-
-    public void addWords(ArrayList<String> Words, int rows, int cols){
+    public void addWords(ArrayList<String> Words){
 	int i = 0;
-	Random R = new Random();
 	int length = Words.size();
 	while(i < length){
-	    int X = R.nextInt(Words.size());
+	    int X = random.nextInt(Words.size());
 	    String word = Words.remove(X);
 	    int q = 0;
-	    while (!(addRandom(word, R.nextInt(rows), R.nextInt(cols))) && (q < 1000000)){
+	    while (!(addRandom(word, random.nextInt(data.length), random.nextInt(data[0].length),
+			       random.nextInt(3) - 1, random.nextInt(3) - 1 )) 
+		   && (q < 1000000)){
 		q = q + 1;}
 	    i = i + 1;}
 	}
+
+    public void fillBlanks(){
+	for(int K = 0; K < data.length; K++){
+	    for(int J = 0; J < data[K].length; J++){
+		if(data[K][J] == ' ')
+		data[K][J] = (char)('A' + (int)(Math.random()*26));
+	    }
+	}
+    }
 
     public static void main(String[]args)throws FileNotFoundException{	
 	File text = new File("WordGrid.txt");
@@ -143,7 +85,10 @@ public class WordGrid{
         }       
 	WordGrid A = new WordGrid(20, 20);
 	System.out.println(A);
-	A.addWords(Words, 20, 20);
-	System.out.println(A);}
+	A.addWords(Words);
+	System.out.println(A);
+	A.fillBlanks();
+	System.out.println(A);
+    }
 
 }
