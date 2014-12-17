c4d6d52ea5dae2e4ed850c3302bf35ba1f1e817f
aaronorelowitz <aaronorelowitz1998@gmail.com>
2014-12-16 22:33:44 -0500

    Update Sorts.java

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,1 +1,16 @@
- 
+public class Sorts{
+ public static void bubble(int[]a) {
+        boolean x = true;                                         
+        while (x) {
+            x = false;
+            for (int i= 1; i < a.length; i ++) {
+                if (a[i-1] > a[i]) {
+                    int foo = a[i];
+                    a[i] = a[i-1];
+                    a[i-1] = foo;
+                    x = true;
+                }
+            }
+        }
+    }
+}
