495156f39c5523f3b71d15abe96541dfda24c1d7
aaronorelowitz <aaronorelowitz1998@gmail.com>
2014-12-17 22:18:00 -0500

    Update Sorts.java

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,5 +1,35 @@
 public class Sorts{
- public static void bubble(int[]a) {
+
+ public static void selection(int[]a) {
+	for (int i = 1; i < a.length; i++){
+	    int x = a[i];
+	    int index = i;
+	    for (int q = i; q < a.length; q++){
+		if (x > (a[q])){
+		    x = a[q];
+		    index = q;
+		}
+	    }
+	    int swap = a[i];
+	    a[i] = x;
+	    a[index] = swap;
+	}
+    }
+
+
+    public static void insertion(int[]a) {
+	for (int i = 1; i < a.length; i++){
+	    int x = a[i];
+	    int j = i;
+	    while (j > 0 && (a[j-1]) > (x)){
+	        a[j] = a[j-1];
+		j = j - 1;}
+	    a[j] = x;}
+
+    }
+
+
+    public static void bubble(int[]a) {
         boolean x = true;                                         
         while (x) {
             x = false;
